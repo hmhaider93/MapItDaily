1686f6b6ec2543ee3eee153314ca56c98b0a5857
Haider Mushtaq <hmhaider93@gmail.com>
2021-02-12 08:26:27 +0100

    enabled user Location tracking

--- app/src/main/java/com/theoutliers/mapitdaily/MapsActivity.kt
+++ app/src/main/java/com/theoutliers/mapitdaily/MapsActivity.kt
@@ -1,7 +1,10 @@
 package com.theoutliers.mapitdaily
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -10,12 +13,18 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,13 +50,39 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         
-        val munich = LatLng(48.1, 11.5)
-        map.addMarker(MarkerOptions().position(munich).title("Marker in Munich"))
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(munich, 12.0f))
+        //val munich = LatLng(48.1, 11.5)
+        //map.addMarker(MarkerOptions().position(munich).title("Marker in Munich"))
+        //map.moveCamera(CameraUpdateFactory.newLatLngZoom(munich, 12.0f))
         map.getUiSettings().setZoomControlsEnabled(true)
         map.setOnMarkerClickListener(this)
+        this.setUpMap()
+
 
     }
 
     override fun onMarkerClick(p0: Marker?): Boolean = false;
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        // 1
+        map.isMyLocationEnabled = true
+
+// 2
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            // Got last known location. In some rare situations this can be null.
+            // 3
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
+
+    }
+
 }
